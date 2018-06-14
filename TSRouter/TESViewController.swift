bdc090d25d7cbff9f0ec8729695eada394d53b2c
yuchenH <huangyuchen@caiqr.com>
2018-06-14 10:57:54 +0800

    commit

---
+++ TSRouter/TESViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TSTestViewController.swift
+//  TSRouter
+//
+//  Created by huangyuchen on 2018/6/7.
+//  Copyright © 2018年 caiqr. All rights reserved.
+//
+import UIKit
+
+class MJNavController: UINavigationController {
+    
+    
+    
+}
+
+class TSTestViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(buttonOnClick), for: UIControlEvents.touchUpInside)
+        view.addSubview(button)
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func buttonOnClick() {
+        
+        TSRouter.openUrl("mjlottery://vc/Home")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ TSRouter/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TSRouter
+//
+//  Created by huangyuchen on 2018/6/6.
+//  Copyright © 2018年 caiqr. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(buttonOnClick), for: UIControlEvents.touchUpInside)
+        view.addSubview(button)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @objc func buttonOnClick() {
+        
+        
+        TSRouter.openUrl("mjlottery://tab4/Home")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
